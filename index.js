02d392867dd075ae104938f574a556a8de65e2e5
rodrigoPgSup <rodrigo.amaral@pagar.me>
2017-08-19 19:54:17 -0300

    wip

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var util = require('util');
+const axios = require('axios');
 
 var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/create_form', function(req, res) {
     res.render('create_form');
@@ -14,5 +16,12 @@ app.get('/create_form', function(req, res) {
 app.get('/show_profile', function(req, res) {
     res.render('show_profile');
 });
+
+app.post('/send_form', function(req, res){
+	axios.post('https://wilkmaia.xyz/forms', {block: req.body})
+	.then((err) => {
+		console.log("Wooo")
+	})
+})
 
 app.listen(3000);
