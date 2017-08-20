d31c0a5efa5fb78b26b8ea6ded61665c4cdc4b59
rodrigoPgSup <rodrigo.amaral@pagar.me>
2017-08-20 02:23:40 -0300

    wip

--- index.js
+++ index.js
@@ -8,21 +8,74 @@ var bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('portraiture'))
 
 app.get('/create_form', function(req, res) {
     res.render('create_form');
 });
 
+app.get('/fill_form', function(req, res) {
+    res.render('form_to_complete');
+});
+
 app.get('/show_profile', function(req, res) {
     res.render('show_profile');
 });
 
+app.get('/show_form/:id', function(req, res) {
+	//include the email also in the query string
+	axios.get(`https://wilkmaia.xyz/forms/${req.params.id}`)
+	.then(function (response) {
+		res.json({
+			error: false,
+			message: response.data
+		})
+	})
+	.catch(function (error) {
+		res.json({
+			error: false,
+			message: error
+		})
+	});
+});
+
 app.post('/send_form', function(req, res){
-    axios.post('https://wilkmaia.xyz/forms', {block: req.body})
-    .then((err) => {
-	    console.log("Wooo")
-    })
-})
+	axios.post('https://wilkmaia.xyz/forms', {block: req.body})
+	.then((err) => {
+		res.render('success')
+	})
+});
+
+app.post('/answer_form', function(req, res){
+	let params = {
+		name: req.body.name,
+		email: req.body.email,
+		github: req.body.github,
+		linkedin: req.body.linkedin
+	}
+	delete req.body.name;
+	delete req.body.email;
+	delete req.body.github;
+	delete req.body.linkedin;
+
+	axios.post('https://wilkmaia.xyz/candidates', {
+		name: params.name,
+		email: params.email,
+		form: req.body,
+		github: params.github,
+		linkedin: params.linkedin
+	})
+	.then((candidate) => {
+		if(candidate.data.error)
+			res.render('form_to_complete')
+		else
+			res.render('success')
+	})
+	.catch(()=>{
+		res.render('form_to_complete')
+	})
+});
+>>>>>>> wip
 
 app.listen(3000);
